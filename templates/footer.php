2958f08a2c965666bb6704b80d903832501e230d
10jotappe <jopedro172@gmail.com>
2026-01-09 21:07:27 -0300

    setup: estrutura inicial e layout base
    Configuração inicial de header, footer e ajustes na página principal de pedidos.

---
+++ templates/footer.php
@@ -0,0 +1,13 @@
+<footer>
+
+    <p>Elo's Pizza &copy; 2026</p>
+
+</footer>
+<!--Bootstrap JS-->
+<script 
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoY9F9U6U6p6tzt+3p6Uib9R5Y5nUj0zYfRvH+8abtTE1Pi"
+    crossorigin="anonymous">
+</script>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link 
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" 
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/php-mysql-crud/CSS/styles.css">
+    <title>Faça seu Pedido</title>
+</head>
+<body>
+    <header>
+
+
+
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand d-flex align-items-center gap-2" href="index.php">
+            <img src="/php-mysql-crud/assets/img/pizza.svg" alt="Elo's Pizza" id="brand-logo">
+            <span class="brand-text text-white">
+                Bem vindo(a)! Peça sua pizza.
+            </span>
+        </a>
+    </nav>
+      <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a href="index.php" class="navbar-brand d-flex align-items-center">
+                <img src="/php-mysql-crud/assets/img/pizza.svg" alt="Elo's Pizza" id="brand-logo">
+            </a>
+
+            <div class="navbar-collapse">
+                <ul class="navbar-nav ms-3">
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link">
+                            Bem vindo(a)! Peça sua pizza.
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav> -->
+    </header>
